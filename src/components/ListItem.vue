fb977b34f8c781910668e124b41bcd2d84a500c8
vicaba <vicaba@me.com>
2023-10-09 19:26:59 +0200

    feat: add vfor view and list item component

---
+++ src/components/ListItem.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  props: {
+    id : Number,
+    name : String,
+    description : String,
+    imageUrl : String
+  }
+}
+</script>
+
+<template>
+
+  <li>
+    <h3>{{ name }}</h3>
+    <p>{{ description }}</p>
+    <img v-bind:src="imageUrl" v-bind:alt="name">
+  </li>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/VFor.vue
@@ -0,0 +1,47 @@
+<script>
+import ListItem from "../components/ListItem.vue";
+
+export default {
+  components: {ListItem},
+  data() {
+    return {
+      listItems: [
+        {
+          id: 1,
+          name: "Item 1",
+          description: "This is the first item",
+          imageUrl: "https://via.placeholder.com/150"
+        },
+        {
+          id: 2,
+          name: "Item 2",
+          description: "This is the second item",
+          imageUrl: "https://via.placeholder.com/150"
+        },
+        {
+          id: 3,
+          name: "Item 3",
+          description: "This is the third item",
+          imageUrl: "https://via.placeholder.com/150"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+
+  <ul>
+    <li v-for="item in listItems" v-bind:key="item.id">
+      <h3>{{ item.name }}</h3>
+      <p>{{ item.description }}</p>
+      <img v-bind:src="item.imageUrl" v-bind:alt="item.name">
+    </li>
+  </ul>
+
+</template>
+
+<style scoped>
+
+</style>
